f1771cc127d750d3d04dbfc1a9119a2feb96df99
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-02-14 16:41:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,11 @@ SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Quizess_prince")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
+#Auto Gcast/Broadcast Handler (True = broadcast on , False = broadcast off During Hosting, Dont Do anything here.)
+AUTO_GCAST = os.getenv("AUTO_GCAST")
 
+#Auto Broadcast Message That You Want Use In Auto Broadcast In All Groups.
+AUTO_GCAST_MSG = getenv("AUTO_GCAST_MSG", "")
 
 # Get this credentials from https://developer.spotify.com/dashboard
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "19609edb1b9f4ed7be0c8c1342039362")
